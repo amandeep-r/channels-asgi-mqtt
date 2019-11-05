b9c6526276f83efd69484829ee33b902df5a3238
Sam Korn <korn94sam@gmail.com>
2019-11-05 11:24:59 -0500

    IoT before

--- tests/mqtt_basic.py
+++ tests/mqtt_basic.py
@@ -2,6 +2,11 @@ import paho.mqtt.client as mqtt
 from time import sleep
 from datetime import datetime
 import json
+import requests
+import logging
+import base64
+import os
+import ssl
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
@@ -23,20 +28,49 @@ def on_disconnect(client, userdata, rc):
 	print("mqtt disconnected...")
 
 
-client = mqtt.Client()
+client = mqtt.Client(client_id="test234")
 client.on_connect = on_connect
 client.on_message = on_message
 client.on_publish = on_publish
 client.on_disconnect = on_disconnect
 
 # mqtt+ssl://
-client.tls_set_context(context=None)
-broker_url = "b-d444068d-a1be-4b51-987e-2026a6ba7768-1.mq.us-east-1.amazonaws.com"
+# Amazon MQ
+# broker_url = "b-d444068d-a1be-4b51-987e-2026a6ba7768-1.mq.us-east-1.amazonaws.com"
+# Amazon IOTCore
+broker_url = "a37viqgs4xlb64-ats.iot.us-east-1.amazonaws.com"
+# Test Unsecured MQTT
 # broker_url = "test.mosquitto.org"
 # broker_port = 1883
 broker_port = 8883
 
-client.username_pw_set("sam", password="testtest1234")
+cert_root = "/home/skorn/cert_test"
+rootCAPath = cert_root+"root-CA.crt"
+certificatePath = cert_root+"pem.crt"
+privateKeyPath = cert_root+"privkey.out"
+
+# Write certs to files, because it needs to be that way
+try:
+    with open(certificatePath,"wb") as f:
+        # print(os.getenv("AWS_CERT"))
+        f.write(base64.b64decode(os.getenv("AWS_CERT")) )
+
+    with open(privateKeyPath,"wb") as f:
+        f.write(base64.b64decode(os.getenv("AWS_PK")) )
+
+    with open(rootCAPath, "w") as f:
+            f.write(requests.get(
+                "https://www.symantec.com/content/en/us/enterprise/verisign/roots/VeriSign-Class%203-Public-Primary-Certification-Authority-G5.pem").text)	
+
+except Exception as e:
+    # logger = logging.getLogger('errors')
+    print("Messenger Error: Certificate and Private Key ENV Variable Missing! %s" % e)
+
+
+# client.tls_set_context(context=None)
+client.tls_set(ca_certs=rootCAPath,certfile=certificatePath, keyfile=privateKeyPath, cert_reqs=ssl.CERT_REQUIRED, tls_version=ssl.PROTOCOL_TLS, ciphers=None)
+# client = os.environ["SERIAL_NUM"] + str(uuid.uuid4().hex)[0:6]
+# client.username_pw_set("sam", password="testtest1234")
 client.connect(broker_url, broker_port, 60)
 sleep(1)
 client.loop_start()
